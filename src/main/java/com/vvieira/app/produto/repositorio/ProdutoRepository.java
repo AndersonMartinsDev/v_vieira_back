2b4d7c07c15d5c8d912006b3b389c1eccc546bb6
Anderson M <foxvc22@gmail.com>
2022-07-08 15:36:41 -0300

    [FIX][0807] Ajuste no ProdutoRepository

--- src/main/java/com/vvieira/app/produto/repositorio/ProdutoRepository.java
+++ src/main/java/com/vvieira/app/produto/repositorio/ProdutoRepository.java
@@ -1,15 +1,8 @@
 package com.vvieira.app.produto.repositorio;
 
-import com.querydsl.core.types.Predicate;
 import com.vvieira.app.produto.entidade.Produto;
-import com.vvieira.app.produto.projections.PProdutoListar;
-import com.vvieira.util.Paginacao;
 import com.vvieira.util.structural.GenericRespository;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface ProdutoRepository extends GenericRespository<Produto,Long> {}
