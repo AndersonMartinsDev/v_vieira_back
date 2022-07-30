0ac197f0d97e8b836f458c22a07095db5a3b6a6b
Anderson M <foxvc22@gmail.com>
2022-07-30 19:57:30 +0200

    [FIX][3007] Ajuste em politica CORS

--- src/main/java/com/vvieira/app/auth/config/Config.java
+++ src/main/java/com/vvieira/app/auth/config/Config.java
@@ -3,10 +3,12 @@ package com.vvieira.app.auth.config;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Data
 @Configuration
-public class Config {
+public class Config implements WebMvcConfigurer {
 
     @Value("${project.version}")
     private String projectVersion;
@@ -19,4 +21,12 @@ public class Config {
 
     @Value("${jwt.secret}")
     private String secret;
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("*/**")
+                .allowedMethods("HEAD","OPTIONS","GET","POST","DELETE","PUT")
+                .allowedHeaders("*");
+    }
 }
